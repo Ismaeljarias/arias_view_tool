f66e8e1a61075bdf3192512ec9070e6294ba735b
Ismael Arias <ismaeljarias@gmail.com>
2017-11-26 01:35:41 -0400

    Arias Tool initial commit

---
+++ lib/arias_view_tool.rb
@@ -0,0 +1,6 @@
+require "arias_view_tool/version"
+require "arias_view_tool/renderer"
+
+module AriasViewTool
+  # Your code goes here...
+end
---
+++ lib/arias_view_tool/version.rb
@@ -0,0 +1,3 @@
+module AriasViewTool
+  VERSION = "0.1.0"
+end
